ec421197f4e0aaeebd65f17d1ba7f6bd37541812
amadou ba <benelafi@gmail.com>
2018-09-18 17:39:24 +0000

    Create list films
    Create 3 films with custom post type
    Adding all needed taxonomy while creating films
    redesign the home page
    add loop to list films on home page with metas

--- wp-content/themes/unite_child/functions.php
+++ wp-content/themes/unite_child/functions.php
@@ -3,3 +3,23 @@
  * @author Papa Amadou adboulaye Ba
  * Custom function for the Unite child theme
  */
+/**
+ * @author Papa Amadou abdoulaye Ba
+ * this function will return the list of a given taxonomy wrapped around  a tags
+ * @param $postID the id of the post type (films)
+ * @param $term the name of the taxonomy
+ *
+ * @return string
+ */
+function films_get_terms( $postID, $term ){
+	$terms_list = wp_get_post_terms($postID, $term);
+	$output = '';
+	$i      = 0;
+
+	foreach( $terms_list as $term ){
+		$i++;
+		if( $i > 1 ){ $output .= ' '; }
+		$output .= '<a class="tag" href="' . get_term_link( $term ) . '">'. $term->name .'</a>';
+	}
+	return $output;
+}
--- wp-content/themes/unite_child/index.php
+++ wp-content/themes/unite_child/index.php
@@ -14,32 +14,35 @@
 get_header(); ?>
 
 <div id="primary" class="content-area col-sm-12 col-md-8">
-	<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
+    <main id="main" class="site-main" role="main">
+    <?php
+        /*
+         * @autho Papa Amadou Abdoulaye Ba
+         * we will list on the home page all saved films ordered by created date DESC
+         */
+		$args = array('post_type' => 'films', 'posts_per_page' => 12, 'order' => 'DESC', );
+        $loop = new WP_Query( $args );
+        if( $loop->have_posts() ){
+			while ( $loop->have_posts() ) { $loop->the_post(); ?>
 				<?php
 				/* Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'content', get_post_format() );
+				get_template_part( 'content', 'films' );
 				?>
 
-			<?php endwhile; ?>
+			<?php }//End of while loop ?>
 
 			<?php unite_paging_nav(); ?>
 
-		<?php else : ?>
+		<?php } else {// PAAB: if no film we display the default page of the parent theme ?>
 
 			<?php get_template_part( 'content', 'none' ); ?>
 
-		<?php endif; ?>
+		<?php }//PAAB: End if loop ?>
 
-	</main><!-- #main -->
+    </main><!-- #main -->
 </div><!-- #primary -->
 
 <?php get_sidebar(); ?>
